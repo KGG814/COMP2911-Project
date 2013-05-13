b77bb839ccaf449da58f4dce7a9e4f1a5dd50a22
Kalana Gamlath <kalana.gamlath1@gmail.com>
2013-05-13 13:52:49 +1000

    Added SudokeSolver

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 COMP2911-Project
 ================
 
-TODO List:
+Separated the solver, gui and board.
+Please add stuff and/or tag your name when you're doing something.
 
-SudokuBoard is quite bloated now, should split solution from the main board function.
+TODO:
 
+<<<<<<< HEAD
 Make GUI 9x9 cells instead of 3x3. Or if we want to do 3x3, abstract it up a level so that the solving algorithms can still operate on the board at a 9x9 level.
 
 8/5/2013 
@@ -14,3 +16,16 @@ Daniel: Fix Up GUI and relate GUI to our initial representation of the board (i.
 Kalana: Get SudokuSolutions and SudokuGenerator Working
 
 Chad: Start Main GUI Layout, without implementation so set Buttons with Labels but no actions. Design some pictures for GUI
+=======
+Have GUI display the values in its cell array
+
+Implement sudoku generator - Kalana
+
+Class diagrams
+
+Menu system
+
+Highscore system
+
+Think of new ideas for GUI
+>>>>>>> Added a SudokuSolver Class
--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,108 +1,17 @@
 
-import java.awt.GridLayout;
-
-import javax.swing.JPanel;
-
-
-public class SudokuBoard extends JPanel {
+public class SudokuBoard {
 		
-   /**
-	* For some reason I have to put an ID
-	*/
-	private static final long serialVersionUID = 1L;
-	
-	Cell[][] boardArray;
-	int[][] solutionArray;
+	int[][] boardArray;
 	
 	public SudokuBoard() {
-		//Set Board Properties
-		setLayout(new GridLayout(9, 9, 1, 1));
-		for(int i = 0; i < 3; i++){
-			for(int j = 0; j < 3; j++) {
-				boardArray[i][j] = new Cell();
-				add(boardArray[i][j]);				
-	 		}
-		}
-		solutionArray = new int[9][9];
+		boardArray = new int[9][9];
 	}
 	void setNumber (int x, int y, int number) {
-		boardArray[x][y].setNumber(number);
+		boardArray[x][y] = number;;
 	}
 
 	int getNumber (int x, int y) {
-		return boardArray[x][y].getNumber();
+		return boardArray[x][y];
 	}
-	
-	public boolean checkRow( int row, int num ) {
-	      for( int i = 0; i < 9; i++ ) {
-	         if( solutionArray[i][row] == num ) {
-	            return false;
-	         }
-	      }
-	      return true;
-	}
-	
-	public boolean checkCol( int col, int num ) {
-	      for( int i = 0; i < 9; i++ ) {
-	         if( solutionArray[col][i] == num ) {
-	            return false;
-	         }
-	      }
-	      return true;
-	}
-	
-   protected boolean checkBox( int col, int row, int num ) {
-      row = (row / 3) * 3 ;
-      col = (col / 3) * 3 ;
-
-      for( int r = 0; r < 3; r++ ) {
-         for( int c = 0; c < 3; c++ ) {
-	         if( solutionArray[col+c][row+r] == num ) {
-	            return false ;
-	         }
-         }
-      }   
-
-      return true ;
-   }
-   
-   public void runSolve ()  {
-	   for( int col = 0; col < 3; col++ ) {
-		   for( int row = 0; row < 3; row++ ) {
-			   solutionArray[col][row] = boardArray[col][row].getNumber();
-		   }
-	   }
-	   try {
-		   solve(0,0);
-	   } catch (Exception e) {
-		   
-	   }
-	
-   }
-   
-   public void solve (int col, int row) throws Exception {
-	   if (row>8) {
-		   throw new Exception("Solution succesful");
-	   }
-	   if (solutionArray[col][row] != 0) {
-		   next(row,col);
-	   } else {
-		   for( int num = 1; num < 10; num++) {
-			   if(checkRow(row,num)&&checkCol(col,num)&&checkBox(col,row,num)) {
-				   solutionArray[col][row] = num;
-				   next(row,col);
-			   }
-		   }
-		   solutionArray[col][row] = 0;
-	   }
-   }
-   
-   public void next (int col, int row) throws Exception {
-	   if (col<8) {
-		   solve(col+1,row);
-	   } else {
-		   solve(0,row+1);
-	   }
-   }
 
 }
