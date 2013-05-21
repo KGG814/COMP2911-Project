4c7158186e59fd08f97ae7df37f4fa4aaaed282e
daniel-soriente <danielsoriente7294@gmail.com>
2013-05-21 13:04:03 +1000

    Update SudokuGUI.java

--- SudokuGUI.java
+++ SudokuGUI.java
@@ -1,19 +1,12 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.Toolkit;
-import java.text.ParseException;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JFormattedTextField;
-import javax.swing.text.DefaultFormatterFactory;
-import javax.swing.text.MaskFormatter;
+
 
 
 /**
@@ -29,31 +22,25 @@ public class SudokuGUI extends JFrame {
 	 * For some reason I have to put an ID
 	 */
 	private static final long serialVersionUID = 1L;
-	JFormattedTextField[][] boardArray;
+	JLabel[][] boardArray;
 	
 	/**
 	 * Puts everything on the JFrame
 	 */
 	public SudokuGUI() {
 
-		boardArray = new JFormattedTextField[9][9];
+		boardArray = new JLabel[9][9];
 		
 		JPanel panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+		//panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
 
-		JPanel sudokuBoard = setBoard();
+		JPanel sudokuBoard = new BoardGUI();
 		JPanel buttons = setButtons();
-		JPanel numbers = setNumButtons();
-		JPanel other = new JPanel();
-
-		other.setLayout(new GridLayout(3, 3));
-		other.add(numbers);
-		other.add(buttons);
-
+		
 		panel.add(sudokuBoard);
-		panel.add(other);
-
+		panel.add(buttons);
 		add(panel);
+		
 		//Set JFrame Properties
 		setLayout(new FlowLayout(FlowLayout.CENTER));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -63,62 +50,6 @@ public class SudokuGUI extends JFrame {
 		setResizable(true);
 		setVisible(true);
 	}
-	
-	public JPanel setBoard() {
-				
-		JPanel board = new JPanel();
-		board.setLayout(new GridLayout(9, 9, 1, 1));
-		
-		for (int col = 0; col < 9; col++){
-			for (int row = 0; row < 9; row++) {
-				
-				boardArray[col][row] = new JFormattedTextField();
-				//JFormattedTextField properties
-				boardArray[col][row].setHorizontalAlignment(javax.swing.JTextField.CENTER);
-				boardArray[col][row].setBorder(BorderFactory.createLineBorder(Color.GRAY));
-				boardArray[col][row].setPreferredSize(new Dimension(35, 35));
-				boardArray[col][row].setFont(new Font("Lucida Console", Font.BOLD, 18));
-				
-				try {
-					boardArray[col][row].setFormatterFactory(new DefaultFormatterFactory(new MaskFormatter("#")));
-		        } catch (java.text.ParseException ex) {}
-				
-				board.add(boardArray[col][row]);
-			}
-		}
-		
-		return board;
-	}
-	
-	public JPanel setNumButtons() {
-
-		JPanel numbers = new JPanel();
-		numbers.setLayout(new GridLayout(3, 3));
-		
-		//will change this later to be more like in the telephone example
-		JButton one = new JButton("1");
-		JButton two = new JButton("2");
-		JButton three = new JButton("3");
-		JButton four = new JButton("4");
-		JButton five = new JButton("5");
-		JButton six = new JButton("6");
-		JButton seven = new JButton("7");
-		JButton eight = new JButton("8");
-		JButton nine = new JButton("9");
-		
-		numbers.add(one);
-		numbers.add(two);
-		numbers.add(three);
-		numbers.add(four);
-		numbers.add(five);
-		numbers.add(six);
-		numbers.add(seven);
-		numbers.add(eight);
-		numbers.add(nine);
-
-		return numbers;
-
-	}
 
 	/**
 	 * 
@@ -126,10 +57,36 @@ public class SudokuGUI extends JFrame {
 	 */
 	public JPanel setButtons() {
 		JPanel buttons = new JPanel();
-
+		buttons.setLayout(new GridLayout(2, 9, 10, 10));
+		Dimension size = new Dimension(90, 40);
+		
 		JButton New = new JButton("New");
+		New.setPreferredSize(size);
+		New.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent event) {
+				//add something
+		    }
+		});
+		
 		JButton GetHint = new JButton("Get Hint");
+		GetHint.setPreferredSize(size);
+		GetHint.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent event) {
+				//add something
+		    }
+		});
+
 		JButton Check = new JButton("Check");
+		Check.setPreferredSize(size);
+		Check.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent event) {
+				//add something
+		    }
+		});
+		
 		buttons.add(New);
 		buttons.add(GetHint);
 		buttons.add(Check);
@@ -137,14 +94,4 @@ public class SudokuGUI extends JFrame {
 		return buttons;
 	}
 
-	public void populateBoard (SudokuBoard board) {
-		for (int col = 0; col<9; col++) {
-			for (int row = 0; row<9; row++){
-				//want to out
-				String number = Integer.toString(board.getNumber(col, row));
-				boardArray[col][row].setText(number);
-				boardArray[0][0].setText("9");
-			}
-		}
-	}
 }
