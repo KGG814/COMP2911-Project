8808f7f76f33e293c5bf18f12cc8ba70bc7dd28c
daniel-soriente <danielsoriente7294@gmail.com>
2013-06-01 23:40:37 +1000

    Update HighGUI.java

--- HighGUI.java
+++ HighGUI.java
@@ -8,43 +8,22 @@ public class HighGUI extends JFrame {
     private static final long serialVersionUID = 1L;
 
     public HighGUI() {
+    	
+        drawList dl = new drawList();
+        add(dl,BorderLayout.CENTER);
 
-        addWindowListener(new WindowAdapter() {
+		addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent we) {
                 dispose();
             }
         });
-        pack();
-        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-        setLocation((d.width / 2), (d.height / 2 - 275));
+		
+		setTitle("High Scores");
+		pack();
+		setSize(400, 500);
+		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation((d.width - 500), (d.height / 2 - 210));
 		setResizable(true);
 		setVisible(true);
-        setSize(400,600);
-//        JPanel but = buttons();
-//        add(but,BorderLayout.SOUTH);
-        drawList dl = new drawList();
-        add(dl,BorderLayout.CENTER);
     }
-/*
-    public JPanel buttons() {
-        JPanel button = new JPanel();
-        button.setLayout(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(5,5,5,5);
-
-        JButton toHome = new JButton("Main menu");
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 2;
-        gbc.fill = GridBagConstraints.BOTH;
-        toHome.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-                dispose();
-            }
-        });
-        button.add(toHome,gbc);
-
-        return button;
-    }
-*/
 }
