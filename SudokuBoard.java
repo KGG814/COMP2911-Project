bd0f07fabe8c2242891285316d0525f7f2eb7292
daniel-soriente <danielsoriente7294@gmail.com>
2013-06-01 23:43:53 +1000

    Update SudokuBoard.java

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -14,7 +14,7 @@ public class SudokuBoard {
 	public SudokuBoard(int[][] boardArray) {
 		this.boardArray = boardArray;
 	}
-	
+
 	void setNumber (int col, int row, int number) {
 		boardArray[col][row] = number;
 	}
@@ -23,20 +23,24 @@ public class SudokuBoard {
 		return boardArray[col][row];
 	}
 
-	boolean isLegalRow (int row, int num) {
-		for (int col = 0; col<9; col++){
-			if (boardArray[col][row]==num){
-				return false;
+	boolean isLegalRow (int row, int col, int num) {
+		for (int c = 0; c<9; c++){
+			if (c != col) {
+				if (boardArray[c][row]==num){
+					return false;
+				}
 			}
+
 		}
 		return true;		
 	}
 
-	boolean isLegalCol (int col, int num) {
-		for (int row = 0; row<9; row++){
-			System.out.print(col);
-			if (boardArray[col][row]==num){
-				return false;
+	boolean isLegalCol (int col, int row, int num) {
+		for (int r = 0; r<9; r++){
+			if(r != row) {
+				if (boardArray[col][r]==num){
+					return false;
+				}
 			}
 		}
 		return true;
@@ -44,23 +48,30 @@ public class SudokuBoard {
 	}
 
 	boolean isLegalBox (int row, int col, int num) {
-		row = (row / 3) * 3 ;
-		col = (col / 3) * 3 ;
+		int x = (row / 3) * 3 ;
+		int y = (col / 3) * 3 ;
 		for( int r = 0; r < 3; r++ ) {
 			for( int c = 0; c < 3; c++ ) {
-				if( boardArray[col+c][row+r] == num ) {
-					return false;
+				if(r != row && c != col) {
+					if( boardArray[y+c][x+r] == num ) {
+						return false;
+					}
 				}
+
 			}
 		}
 		return true;
 	}
 
 	boolean numberIsLegal (int row, int col, int num) {
-		System.out.println(num);
-		if (isLegalRow(row,num)&&isLegalCol(col,num)&&isLegalBox(row,col,num)) {
+		//System.out.println(row);
+		//System.out.println(col);
+		System.out.println(isLegalCol(col, row, num));
+		System.out.println(isLegalRow(row, col, num));
+		System.out.println(isLegalBox(row, col, num));
+		if (isLegalRow(row, col, num)&&isLegalCol(col, row, num)&&isLegalBox(row,col,num)) {
 			return true;
-		} else if (num==0){
+		} else if (num == 0) {
 			return true;
 		} else {
 			return false;
