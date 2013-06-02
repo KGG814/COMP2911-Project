feaf454f1196b6d9b8cebd0741a8433d9ed80839
daniel-soriente <danielsoriente7294@gmail.com>
2013-06-02 23:38:16 +1000

    Update SudokuBoard.java

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -2,7 +2,11 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-
+/**
+ * Class to represent the sudokuboard
+ * back-end only stores the number in a 2dim array
+ * if there is nothing in the cell it is 0
+ */
 public class SudokuBoard {
 
 	int[][] boardArray;
@@ -15,14 +19,33 @@ public class SudokuBoard {
 		this.boardArray = boardArray;
 	}
 
+	/**
+	 * Set the specified col and row with the specified number on the board
+	 * @param col
+	 * @param row
+	 * @param number
+	 */
 	void setNumber (int col, int row, int number) {
 		boardArray[col][row] = number;
 	}
 
+	/**
+	 * Get the specified number in the cell
+	 * @param col
+	 * @param row
+	 * @return
+	 */
 	int getNumber (int col, int row) {
 		return boardArray[col][row];
 	}
 
+	/**
+	 * Check is the number entered is legal in the row
+	 * @param row
+	 * @param col
+	 * @param num
+	 * @return
+	 */
 	boolean isLegalRow (int row, int col, int num) {
 		for (int c = 0; c<9; c++){
 			if (c != col) {
@@ -35,6 +58,13 @@ public class SudokuBoard {
 		return true;		
 	}
 
+	/**
+	 * Checks if the number entered is legal in the column
+	 * @param col
+	 * @param row
+	 * @param num
+	 * @return
+	 */
 	boolean isLegalCol (int col, int row, int num) {
 		for (int r = 0; r<9; r++){
 			if(r != row) {
@@ -47,6 +77,13 @@ public class SudokuBoard {
 
 	}
 
+	/**
+	 * Checks if the number entered is correct in the square
+	 * @param row
+	 * @param col
+	 * @param num
+	 * @return
+	 */
 	boolean isLegalBox (int row, int col, int num) {
 		int x = (row / 3) * 3 ;
 		int y = (col / 3) * 3 ;
@@ -63,6 +100,14 @@ public class SudokuBoard {
 		return true;
 	}
 
+	/**
+	 * Checks if the number entered is correct in the row, column and square
+	 * used the methods above
+	 * @param row
+	 * @param col
+	 * @param num
+	 * @return
+	 */
 	boolean numberIsLegal (int row, int col, int num) {
 		if (isLegalRow(row, col, num)&&isLegalCol(col, row, num)&&isLegalBox(row,col,num)) {
 			return true;
@@ -73,6 +118,10 @@ public class SudokuBoard {
 		}
 	}
 
+	/** 
+	 * Prints the board onto the console
+	 * Used for testing
+	 */
 	public void printBoard() {
 		for (int row=8; row>=0;row--){
 			for (int col=0; col<9;col++){
@@ -83,6 +132,14 @@ public class SudokuBoard {
 		}
 	}
 
+	/**
+	 * Saves the state of the board into a file called save.txt
+	 * @param difficulty
+	 * @param time
+	 * @param hints
+	 * @param editableArray
+	 * @throws IOException
+	 */
 	public void saveState(int difficulty, int time, int hints,
 			boolean[][] editableArray) throws IOException {
 		File save = new File("save.txt");
