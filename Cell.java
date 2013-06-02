00a7e36ced36f994e347d4ad4855679e839fa388
daniel-soriente <danielsoriente7294@gmail.com>
2013-06-02 22:42:42 +1000

    Update Cell.java

--- Cell.java
+++ Cell.java
@@ -4,55 +4,66 @@ import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+/**
+ * Class used to represent a cell on the board
+ * acts as a way to display the number
+ * Stores where in the board it is
+ * @author Daniel
+ *
+ */
 public class Cell extends JLabel {
-
+  
     private static final long serialVersionUID = 1L;
-    private boolean editable;
-    private final int col;
-    private final int row;
-    private final boolean onGrey;
+	private boolean editable;
+	private final int col;
+	private final int row;
 
-    public Cell(int col, int row) {
-        this.editable = true;
-        this.col = col;
-        this.row = row;
-        this.onGrey = (col > 2 && col < 6 && row < 3)
-            ||(row > 2 && row < 6 && col < 3)
-            ||(col > 2 && col < 6 && row > 5)
-            ||(row > 2 && row < 6 && col > 5);
-        setHorizontalAlignment(JTextField.CENTER);
-        setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
-        setPreferredSize(new Dimension(40, 40));
-        setFont(new Font("Lucida Console", Font.BOLD, 18));
-        setForeground(Color.BLACK);
-        if(this.isGrey()) {
-            setBackground(new Color(240,240,240));
-        } else {
-            setBackground(Color.WHITE);
-        }
-        setOpaque(true);
-    }
+	public Cell(int col, int row) {
+		this.editable = true;
+		this.col = col;
+		this.row = row;
+		setHorizontalAlignment(JTextField.CENTER);
+		setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
+		setPreferredSize(new Dimension(40, 40));
+		setFont(new Font("Arial", Font.BOLD, 18));
+		setForeground(Color.BLACK);
+		setBackground(Color.WHITE);
+		setOpaque(true);
+	}
 
-    public void setEditable(boolean status) {
-        editable = status;
-        if(!status) {
-            setForeground(Color.GRAY);
-        }
-    }
+	/**
+	 * Set if the cell can be changed
+	 * If it cant be changed, set the colour to gray
+	 * @param status
+	 */
+	public void setEditable(boolean status) {
+		editable = status;
+		if(!status) {
+			setForeground(Color.GRAY);
+		}
+	}
 
-    public boolean getEditable() {
-        return editable;
-    }
+	/**
+	 * 
+	 * @return if editabl or not
+	 */
+	public boolean getEditable() {
+		return editable;
+	}
 
-    public int getCol() {
-        return col;
-    }
+	/**
+	 * Get the column number the cell is in
+	 * @return
+	 */
+	public int getCol() {
+		return col;
+	}
 
-    public int getRow() {
-        return row;
-    }
-
-    public boolean isGrey() {
-        return onGrey;
-    }
+	/**
+	 * Get the row number the cell is in
+	 * @return
+	 */
+	public int getRow() {
+		return row;
+	}
 }
