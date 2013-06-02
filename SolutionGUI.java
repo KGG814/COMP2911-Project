3f72271aba0785e205d385b14fb75d567cec966a
Kalana Gamlath <kalana.gamlath1@gmail.com>
2013-06-02 17:05:06 +1000

    Added UML Diagram

--- SolutionGUI.java
+++ SolutionGUI.java
@@ -8,12 +8,10 @@ import javax.swing.JPanel;
 public class SolutionGUI extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private BoardGUI solutionBoard;
-	private SudokuBoard solution;
-
+	
 	public SolutionGUI(SudokuBoard solution) {
 		JPanel panel = new JPanel();
 		//panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
-		this.solution = solution;
 		solutionBoard = new BoardGUI(solution);
 		panel.add(solutionBoard);
 		add(panel);	
@@ -30,15 +28,5 @@ public class SolutionGUI extends JFrame {
 		setSize(400, 425);
 		setResizable(true);
 		setVisible(true);
-	}
-
-	public void populateBoard () {
-		for (int col = 0; col<9; col++) {
-			for (int row = 0; row<9; row++){
-				//want to out
-				String number = Integer.toString(solution.getNumber(col, row));
-				solutionBoard.board[col][row].setText(number);
-			}
-		}
 	}
 }
